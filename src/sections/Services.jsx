1d516b5bf781e8bdac9acee0e0875a425bc2b012
MichealDavid1 <madtechnow@gmail.com>
2024-01-03 22:44:42 +0100

    completed service page

--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,5 +1,13 @@
+import { services } from "../constants";
+import ServiceCard from "../components/ServiceCard";
 const Services = () => {
-  return <div>Services</div>;
+  return (
+    <div className="max-container flex justify-center flex-wrap gap-9">
+      {services.map((value) => (
+        <ServiceCard key={value.label} {...value} />
+      ))}
+    </div>
+  );
 };
 
 export default Services;
