62c3c54069f7d2f7ed3fcf16048e09e7df8f0ba0
MichealDavid1 <madtechnow@gmail.com>
2024-01-03 18:02:14 +0100

    stoped at super quality  page

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -13,20 +13,22 @@ const Hero = () => {
         <p className="font-montserrat text-xl text-coral-red">
           Our Summer Collection
         </p>
-        <h1 className="font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82] font-bold mt-10">
+        <h1 className="font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold mt-10">
           <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">
             The new arrival
           </span>
           <br />
-          <span className="text-coral-red inline-block mt-3">Nike</span> Shoes
+          <span className="whitespace-nowrap">
+            <span className="text-coral-red inline-block mt-3">Nike</span> Shoes
+          </span>
         </h1>
         <p className="font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm">
           discover stylish nike arrivals, quality comfort, and innovation for
           you active life.
         </p>
         <Button label="Shop Now" iconLink={arrowRight} />
-        <div className="flex justify-start items-start flex-wrap w-full mt-20 gap-16">
-          {statistics.map((value, index) => (
+        <div className="flex justify-start items-start sm:flex-wrap w-full mt-20 gap-16">
+          {statistics.map((value) => (
             <div key={value.label}>
               <p className="text-4xl font-palanquin font-bold">{value.value}</p>
               <p className="leading-7 font-montserrat text-slate-gray">
@@ -44,7 +46,7 @@ const Hero = () => {
           height={500}
           className="object-contain relative z-10"
         />
-        <div className="flex sm:gap-6 gap-4 absolute -bottom-[6%] sm:left-[10%] max-sm:px-6">
+        <div className="flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6">
           {shoes.map((value) => (
             <div key={value}>
               <ItemCard
--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -1,5 +1,39 @@
+import Button from "../components/Button";
+import { shoe8 } from "../assets/images";
 const SuperQuality = () => {
-  return <div>SuperQuality</div>;
+  return (
+    <div
+      id="about-us"
+      className="flex justify-between items-center max-lg:flex-col gap-10 w-full max-container"
+    >
+      <div className="flex flex-1 flex-col">
+        <h2 className="font-palanquin text-4xl  capitalize font-bold mt-10 lg:max-w-lg">
+          We provide you <span className="text-coral-red">super</span>{" "}
+          <span className="text-coral-red">quality</span> shoes
+        </h2>
+        <p className="mt-4 lg:max-w-lg info-text capitalize">
+          ensuring premium comfort and style, our meticulously crafted footware
+          is designed to elevate your experience, providing you with unmatched
+          quality, innovation, and a touch of elegance.
+        </p>
+        <p className="mt-6 lg:max-w-lg info-text capitalize">
+          our dedication to detail and excellence ensures your satisfaction.
+        </p>
+        <div className="mt-11">
+          <Button label="View Details" />
+        </div>
+      </div>
+      <div className="flex-1 flex justify-center items-center">
+        <img
+          src={shoe8}
+          alt="A shoe"
+          width={570}
+          height={522}
+          className="object-contain"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default SuperQuality;
