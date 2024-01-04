749a54dc5f6a100a8928f9d66069fc05ed20f078
MichealDavid1 <madtechnow@gmail.com>
2024-01-04 21:46:33 +0100

    complete customer review section

--- src/components/ReviewCard.jsx
+++ src/components/ReviewCard.jsx
@@ -8,8 +8,8 @@ const ReviewCard = ({ imgURL, customerName, rating, feedback }) => {
         alt="customer"
         className="rounded-full object-cover w-[120px] h-[120px]"
       />
-      <p>{feedback}</p>
-      <div>
+      <p className="mt-6 max-w-sm text-center info-text">{feedback}</p>
+      <div className="mt-3 flex justify-center items-center gap-2">
         <img
           src={star}
           alt="Star Ratings"
@@ -17,8 +17,11 @@ const ReviewCard = ({ imgURL, customerName, rating, feedback }) => {
           height={25}
           className="object-contain m-0"
         />
-        <p>({rating})</p>
+        <p className="text-xl font-montserrat text-slate-gray">({rating})</p>
       </div>
+      <h3 className="mt-1 font-palanquin text-3xl text-center font-bold">
+        {customerName}
+      </h3>
     </div>
   );
 };
