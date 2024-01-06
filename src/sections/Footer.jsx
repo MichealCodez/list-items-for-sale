ce750f339b28a27cd77d1dff6531d09d58b19c9a
MichealDavid1 <madtechnow@gmail.com>
2024-01-06 22:04:06 +0100

    prepared footer section

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <footer className="max-container">
       <div className="flex justify-between items-start gap-20 flex-wrap max-lg:flex-col">
-        <div></div>
+        <div className=""></div>
       </div>
     </footer>
   );
