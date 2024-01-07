7ac2bc6bfd3d7f8bc1d6b8e4b95c9ee52594b80b
MichealDavid1 <madtechnow@gmail.com>
2024-01-07 19:31:34 +0100

    testing stuff

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,11 +1,17 @@
+import { footerLogo } from "../assets/images";
+
 const Footer = () => {
   return (
     <footer className="max-container">
       <div className="flex justify-between items-start gap-20 flex-wrap max-lg:flex-col">
         <div className="flex flex-col items-start">
           <a href="/">
-            <img src={footerLogo} alt="Logo" />
+            <img src={footerLogo} alt="Logo" width={150} height={46} />
           </a>
+          <p className="capitalize info-text">
+            get shoes ready for the new term at your nearest nike store. find
+            your perfect size in stor. Get rewards
+          </p>
         </div>
       </div>
     </footer>
